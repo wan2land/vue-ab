8a70d30eee7e26ed6662222a6eb2e49b92f11d69
Changwan Jun <im@wani.kr>
2017-09-29 18:09:05 +0900

    add weight to vue-ab

--- example-src/App.vue
+++ example-src/App.vue
@@ -1,32 +1,77 @@
+<style scoped>
+    .demo {
+        padding: .5rem;
+        background: #F8F8F8;
+    }
+</style>
 <template>
     <div id="app" class="container pt-4">
-        <h4>Simple Example</h4>
-        <h5>Demo.</h5>
+        <h1>Vue AB</h1>
         <div>
-            <button type="button" class="btn btn-primary" @click="resample">ReSample</button>
-        </div>
-        <Ab @sample="sample" ref="ab">
-            <div slot="candidate1">
-                Candidate 1, Div :-)
+            <h4 class="mt-4">Basic</h4>
+            <hr />
+            <h5 class="mt-4">Demo</h5>
+            <div>
+                <button type="button" class="btn btn-primary" @click="$refs.ab1.resample()">Re-Sample</button>
+                <button type="button" class="btn btn-primary" @click="cleanCookie">Clean All Cookie</button>
             </div>
-            <p slot="candidate2">
-                Candidate 2, p :-)
-            </p>
-            <template slot="candidate3">
-                Candidate 3, String :-)
-            </template>
-            <template slot="candidate4">
-                <div>Candidate 4</div>
-                <p>Has Many Children :-)</p>
-            </template>
-            <!-- nothing :-) -->
-            <template slot="candidate5"></template>
-        </Ab>
-
-        <h5>Example.</h5>
-
+            <div class="candidate">candidate = <strong>{{ candidate1 }}</strong></div>
+            <div class="demo mt-4">
+                <Ab @sample="(candidate) => candidate1 = candidate" cookie-name="vue-ab-example1" ref="ab1">
+                    <div slot="candidate1">
+                        Candidate 1, Div :-)
+                    </div>
+                    <p slot="candidate2">
+                        Candidate 2, p :-)
+                    </p>
+                    <template slot="candidate3">
+                        Candidate 3, String :-)
+                    </template>
+                    <template slot="candidate4">
+                        <div>Candidate 4</div>
+                        <p>Has Many Children :-)</p>
+                    </template>
+                    <!-- nothing :-) -->
+                    <template slot="candidate5"></template>
+                </Ab>
+            </div>
+            <h5 class="mt-4">Code</h5>
+            <div>
+                <pre><code class="html" ref="example1"></code></pre>
+            </div>
+        </div>
         <div>
-            <pre><code class="html" ref="example1"></code></pre>
+            <h4 class="mt-4">Weight</h4>
+            <hr />
+            <h5 class="mt-4">Demo</h5>
+            <div>
+                <button type="button" class="btn btn-primary" @click="$refs.ab2.resample()">Re-Sample</button>
+                <button type="button" class="btn btn-primary" @click="cleanCookie">Clean All Cookie</button>
+            </div>
+            <div class="candidate">candidate = <strong>{{ candidate2 }}</strong></div>
+            <div class="demo mt-4">
+                <Ab @sample="(candidate) => candidate2 = candidate" cookie-name="vue-ab-example2" :weight="{candidate1: 100, candidate2: 100, candidate3: 10, candidate4: 10}" ref="ab2">
+                    <div slot="candidate1">
+                        Candidate 1, (weight = 100)
+                    </div>
+                    <div slot="candidate2">
+                        Candidate 2, (weight = 100)
+                    </div>
+                    <div slot="candidate3">
+                        Candidate 3, (weight = 10)
+                    </div>
+                    <div slot="candidate4">
+                        Candidate 4, (weight = 10)
+                    </div>
+                    <div slot="candidate5">
+                        Candidate 5, (weight = 0), Never
+                    </div>
+                </Ab>
+            </div>
+            <h5 class="mt-4">Code</h5>
+            <div>
+                <pre><code class="html" ref="example2"></code></pre>
+            </div>
         </div>
     </div>
 </template>
@@ -34,7 +79,12 @@
     export default {
         data() {
             return {
-                example1: `<Ab>
+                candidate1: null,
+                candidate2: null,
+                example1: `<Ab
+    @sample="(candidate) => candidate1 = candidate"
+    cookie-name="vue-ab-example1"
+>
     <div slot="candidate1">
         Candidate 1, Div :-)
     </div>
@@ -50,20 +100,43 @@
     </template>
     <!-- nothing :-) -->
     <template slot="candidate5"></template>
-</Ab>
-`,
+</Ab>`,
+                example2: `<Ab
+    @sample="(candidate) => candidate2 = candidate"
+    cookie-name="vue-ab-example2"
+    :weight="{candidate1: 100, candidate2: 100, candidate3: 10, candidate4: 10}"
+>
+    <div slot="candidate1">
+        Candidate 1, (weight = 100)
+    </div>
+    <div slot="candidate2">
+        Candidate 2, (weight = 100)
+    </div>
+    <div slot="candidate3">
+        Candidate 3, (weight = 10)
+    </div>
+    <div slot="candidate4">
+        Candidate 4, (weight = 10)
+    </div>
+    <div slot="candidate5">
+        Candidate 5, (weight = 0), Never
+    </div>
+</Ab>`,
             };
         },
         mounted() {
             this.$refs.example1.textContent = this.example1;
             hljs.highlightBlock(this.$refs.example1);
+            this.$refs.example2.textContent = this.example2;
+            hljs.highlightBlock(this.$refs.example2);
         },
         methods: {
-            sample(candidate) {
-                console.log("sample!", candidate);
-            },
-            resample() {
-                this.$refs.ab.resample();
+            cleanCookie() {
+                document.cookie.split(";").forEach((c) => {
+                    document.cookie = c
+                        .replace(/^ +/, "")
+                        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+                });
             },
         },
     };
--- src/Ab.js
+++ src/Ab.js
@@ -1,17 +1,32 @@
 
 import Cookie from 'js-cookie'
 
-function pick(collection) {
+function pick(collection, weight = {}) {
     var keys = Object.keys(collection);
+    var thresholds = [];
+    var sumOfWeights = 0;
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        let currWeight = weight[key];
+        if (!currWeight) continue;
+        thresholds.push([key, sumOfWeights + currWeight]);
+        sumOfWeights += currWeight;
+    }
+    if (sumOfWeights) {
+        var pickedKey = Math.floor(Math.random() * sumOfWeights);
+        for (let i = 0; i < thresholds.length; i++) {
+            if (thresholds[i][1] > pickedKey) return thresholds[i][0];
+        }
+    }
     return keys[Math.floor(Math.random() * keys.length)];
 }
-
 export default {
     props: {
         tag: {
             type: String,
             default: "div",
         },
+        weight: Object,
         cookieName: {
             type: String,
             default: "vue-ab-cookie",
@@ -28,13 +43,15 @@ export default {
         if (restoredCandidate) {
             candidate = restoredCandidate;
         } else {
-            candidate = pick(this.$slots);
+            candidate = pick(this.$slots, this.weight);
             Cookie.set(this.cookieName, candidate, {expires: this.cookieExpires});
-            this.$nextTick(() => this.$emit('sample', candidate));
         }
         return {
             candidate: candidate,
         };
+    },
+    mounted() {
+        this.$emit('sample', this.candidate);
     },
     render(h) {
         let candidateSlot = this.$slots[this.candidate];
@@ -45,7 +62,7 @@ export default {
     },
     methods: {
         resample() {
-            var candidate = pick(this.$slots);
+            var candidate = pick(this.$slots, this.weight);
             this.$nextTick(() => this.$emit('sample', candidate));
             Cookie.set(this.cookieName, candidate, {expires: this.cookieExpires});
             this.candidate = candidate;
